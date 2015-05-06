87ab6ccc0bbbe34dfcc13d54beae42628cf0fb56
Tom Screen <tomscreen@msn.com>
2015-05-06 17:46:58 +0100

    update

--- sidemo/wss.js
+++ sidemo/wss.js
@@ -1,12 +1,10 @@
-var express = require('express')
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ port: 8080 });
 
-// var WebSocketServer = require('ws').Server
-//   , wss = new WebSocketServer({ port: 8080 });
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
 
-// wss.on('connection', function connection(ws) {
-//   ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-//   });
-
-//   ws.send('something');
-// });
+  ws.send('something');
+});
